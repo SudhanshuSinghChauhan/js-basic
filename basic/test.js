d54601902f7923a21419ca50eecaf37b4b546688
SudhanshuSinghChauhan <99023909+SudhanshuSinghChauhan@users.noreply.github.com>
2024-05-15 19:00:33 +0000

    cha

---
+++ basic/test.js
@@ -0,0 +1,16 @@
+const namea=32233 //mostly used coz it cannot be changed
+let abcd = "hello"
+var acc = "1233"
+
+console.log(namea)
+
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score))
+
+let vlaue = Number(score)
+console.log(typeof(vlaue)) 
+console.log(vlaue)
+
+// data types -> primitive -> 7 types (all call by value [copy me changes hote hai]) String,Number Boolean, null, undefined, Symbol, BigInt
+// non primitive -> reference type -> array, Objects, Functions
